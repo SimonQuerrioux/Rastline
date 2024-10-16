3566931117e6cbb66614ebd50138c6ac33d97c30
SimonQuerrioux <sim.querrioux@gmail.com>
2024-10-16 19:39:36 +0200

    16.10.2024

---
+++ src/routes/rastline/+page.svelte
@@ -0,0 +1,46 @@
+<!--Naslov-->
+<h1>Predstavitev rastlin</h1>
+<div>
+	<!--Kaj so ratline-->
+	<p>
+		Rastlíne (znanstveno ime Plantae) so eno izmed kraljestev živih bitij, v katerega uvrščamo
+		približno 300.000 danes znanih vrst. Skupina vključuje splošno razširjene in poznane življenjske
+		oblike, kot so drevesa, cvetnice, trave, praprotnice in mahovi, izključuje pa rdeče in rjave
+		alge, živali, glive, arheje ter bakterije. Za rastline je značilno, da razen izjem pridobivajo
+		energijo za rast in delovanje organizma iz sončne svetlobe s pomočjo procesa fotosinteze, ki
+		poteka v kloroplastih, poleg tega imajo njihove celice celične stene, grajene iz celuloze. Z
+		rastlinami se znanstveno ukvarja botanika, ki je panoga biologije. V preteklosti so med rastline
+		uvrščali tudi alge, glive in druge nesorodne avtotrofne organizme, to tradicijo botanika do neke
+		mere ohranja še danes. Skupnosti vseh rastlin na določenem območju v določenem času pravimo
+		flora.
+	</p>
+</div>
+<br />
+<!--Tri rastline-->
+<div class="grid grid-cols-3 gap-5 p-5">
+	<!--Malina-->
+	<div class=" bg-orange-400 flex flex-col items-center p-5 rounded-2xl">
+		<h3>Malina</h3>
+		<!-- svelte-ignore a11y-missing-attribute -->
+		<img
+			class="w-1/1.5 rounded-2xl"
+			src="https://stanic.co.rs/wp-content/uploads/2020/03/Stanic-Malina-1.jpg"
+		/>
+		<button>Preberi več</button>
+	</div>
+	<!--Breza-->
+	<div class=" bg-orange-400 flex flex-col items-center p-5 rounded-2xl">
+		<h3>Breza</h3>
+		<!-- svelte-ignore a11y-missing-attribute -->
+		<img class="w-1/1.5 rounded-2xl" src="https://www.gobe.si/slike/navadna-breza.jpg" />
+	</div>
+	<!--Goba-->
+	<div class=" bg-orange-400 flex flex-col items-center p-5 rounded-2xl">
+		<h3>Goba</h3>
+		<!-- svelte-ignore a11y-missing-attribute -->
+		<img
+			class="w-1/1.5 rounded-2xl"
+			src="https://1001ideja.si/wp-content/uploads/2022/09/Gabrov-dedek.jpg"
+		/>
+	</div>
+</div>
